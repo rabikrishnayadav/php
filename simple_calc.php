73de6a687c7a98e955913cf8d4a3e210447c370d
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-04 00:14:34 +0545

    simple calculator

---
+++ simple_calc.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Simple Calculator</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	</head>
+	<body class="bg-success">
+		<div class="container">
+			<div class="bg-dark m-5 p-5 rounded">
+				<div class="row">
+					<div class="col-xl-6 font-weight-bold">
+						<div class="app-title">
+							<div class="animated infinite pluse text-success">
+								PHP <br> Calculator <br>
+							</div>
+						</div>
+					</div>
+					<div class="col-xl-6">
+						<div class="screen-body-item">
+							<div class="app-form">
+								<form method="POST">
+									<div class="app-form-group">
+										<input type="text" name="num1" placeholder="Enter First Number" class="app-form-control">
+									</div><br>
+									<div class="app-form-group">
+										<input type="text" name="num2" placeholder="Enter Second Number" class="app-form-control">
+									</div><br>
+									<div class="select-style">
+										<select name="operation">
+											<option value="add">Add</option>
+											<option value="sub">Sub</option>
+											<option value="mult">Mult</option>
+											<option value="div">Div</option>
+										</select>
+									</div><br>
+									<div>
+										<input type="submit" name="submit" value="Calculate" class="app-form-button btn btn-success">
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="app-form-group showdata text-light text-center p-5">
+					<p>
+						<?php
+						
+						if (isset($_POST['submit'])) {
+							$num1 = $_POST['num1'];
+							$num2 = $_POST['num2'];
+							$operation = $_POST['operation'];
+							switch ($operation) {
+								case 'add': $sum = $num1 + $num2;
+									echo "The Addition of Two number is {$sum}";
+									break;
+								case 'sub': $sub = $num1 + $num2;
+									echo "The Subtraction of Two number is {$sub}";
+									break;
+								case 'mult': $mult = $num1 * $num2;
+									echo "The Multiplication of Two number is {$mult}";
+									break;
+								case 'div': $div = $num1 / $num2;
+									echo "The Division of Two number is {$div}";
+									break;
+								
+								default: echo 'Please Select the operation';
+							}
+						}
+						?>
+					</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
