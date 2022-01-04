b23a4107b0567f8461c419d793b3060e2e0078e2
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-04 11:30:43 +0545

    FIZZ BUZZ program

--- index_second.php
+++ index_second.php
@@ -71,6 +71,40 @@
 				</h2>
 				Just refresh the brower for another number.
 			</div><br><hr><br>
+			<div>
+				<h3>FIZZ BUZZ Program</h3>
+				Example:<br>
+				if a number is a multiple of 3 then output FIZZ instead of the number.<br>
+				if a number is a multiple of 5 then output BUZZ instead of the number.<br>
+				if a number is a multiple of both 3 and 5 then output FIZZ BUZZ instead of the number.<br>
+				<code>
+				for ($i=1; $i<=20; $i++) { <br>
+					if (($i%3 == 0) && ($i%5 == 0)) {<br>
+						echo "FIZZ BUZZ"; <br>
+					}elseif ($i%3 == 0) {<br>
+						echo "FIZZ"; <br>
+					}elseif ($i%5 == 0){<br>
+						echo "BUZZ"; <br>
+					}else{<br>
+						echo $i.";<br>
+					}<br>
+				}	<br>
+				</code><br>
+				<kbd>Result:</kbd><br>
+				<?php
+				for ($i=1; $i<=20; $i++) { 
+					if (($i%3 == 0) && ($i%5 == 0)) {
+						echo "FIZZ BUZZ <br>";
+					}elseif ($i%3 == 0) {
+						echo "FIZZ <br>";
+					}elseif ($i%5 == 0){
+						echo "BUZZ <br>";
+					}else{
+						echo $i. "<br>";
+					}
+				}
+				?>
+			</div><br><hr><br>
 		</div>
 	</body>
 </html>
