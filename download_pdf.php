a9f885461e065c15af74f7c4f7ede9a83abb3cbe
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-07 15:35:04 +0545

    pdf file download function

---
+++ download_pdf.php
@@ -0,0 +1,14 @@
+<?php
+
+$file = $_GET['file'].".pdf";
+
+header("content-disposition: attachment; filename=" .urlencode($file));
+
+$fb = fopen($file, "r");
+
+while(!feof($fb)) {
+	echo fread($fb, 8192);
+	flush();
+}
+fclose($fb);
+?>
---
+++ pdf_file.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>All About Pdf File</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+</head>
+<body>
+	<h1 align="center">All About Pdf File Download and View</h1>
+<div class="container"><hr>
+	<h5>Simple Method of download the pdf file</h5>
+	<p align="center">
+		<b>Click below to download PDF</b><br>
+		<a href="html_tutorial.pdf" target="_blank">Download Pdf Now</a>
+	</p><hr>
+	<h5>With the help of Php Method</h5>
+	<p align="center">
+		<b>Click below to download PDF</b><br>
+		<a href="download_pdf.php?file=html_tutorial" target="_blank">Download Pdf Now</a>
+	</p>
+</div>
+</body>
+</html>
