d4856e906090d699225df9ea049190baa9ce9d3d
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-11 09:03:20 +0545

    oops star

---
+++ oops/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Oops</title>
+	<?php include '../user/link.php' ?>
+</head>
+<body>
+	<h1 class="text-center font-weight-bold bg-warning">Object Oriented PHP</h1>
+	<div class="container">
+		<ul>
+		<h3>Some Topics Of Object Oriented php</h3>
+		<li>Class</li>
+		<li>Object</li>
+		<li>Inheritance</li>
+		<li>Constructor</li>
+		<li>Method Overriding</li>
+		<li>Interface</li>
+		<li>Abstract Class & Method</li>
+		<li>Access Modifiers</li>
+	</ul>
+	</div>
+</body>
+</html>
